8fd6822fca26f6bb7f5729724b644b3e68789d04
Aryan Padmakar Shirsat <aryanshirsat357@gmail.com>
2024-01-15 19:09:27 +0530

    skills section updated

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div className='p-5 justify-center'>
+      <h2 className='text-5xl font-extrabold bg-gradient-to-br from-pink-500 to-blue-700 text-transparent bg-clip-text'>Skills</h2>
+    </div>
   )
 }
 
